21c6865d8ecabc8b07d0ad21eb961cecf25977bf
CiaranMc93 <C11354741@mydit.ie>
2016-06-07 07:38:48 +0100

    Build all android files/dependencies

---
+++ app/src/main/java/com/ciacavus/tapreact/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ciacavus.tapreact;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
