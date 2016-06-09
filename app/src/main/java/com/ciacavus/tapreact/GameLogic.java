a762b4f2e204cbdad8006e2ed130c3b32afd86b1
CiaranMc93 <C11354741@mydit.ie>
2016-06-09 07:42:22 +0100

    Timer works but the gesture listener does not(Need to revise). Get started on changing the 'Action' every few seconds and speeding up the timer.

--- app/src/main/java/com/ciacavus/tapreact/GameLogic.java
+++ app/src/main/java/com/ciacavus/tapreact/GameLogic.java
@@ -1,13 +1,20 @@
 package com.ciacavus.tapreact;
 
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.SystemClock;
+import android.os.Vibrator;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import org.junit.rules.Stopwatch;
@@ -17,6 +24,8 @@ import org.junit.rules.Stopwatch;
  */
 public class GameLogic extends AppCompatActivity implements GestureDetector.OnDoubleTapListener, GestureDetector.OnGestureListener, SensorEventListener {
 
+    //create gesture variable
+    GestureDetector mDetector;
     //set up the global variables to manage framerate etc
     private final static int MAX_FPS = 50;
     //num frames to be skipped
@@ -26,18 +35,81 @@ public class GameLogic extends AppCompatActivity implements GestureDetector.OnDo
     //string to hold the game options and let the user know
     public String[] gameOptions = {"twistIt","tapIt","turnIt"};
 
+    //main timer
+    private long startTime = 0L;
+    //handle the time
+    private Handler customHandler = new Handler();
+    //time logic
+    long timeInMilliseconds = 0L;
+    long timeSwapBuff = 0L;
+    long updatedTime = 0L;
+    int minutes;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.game_layout);
 
+        //keep the screen on so the person can not be confused if their screen dims
+        //sourced from http://stackoverflow.com/questions/4195682/android-disable-screen-timeout-while-app-is-running
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        //start the timer and make play button invisible
+        startTime = SystemClock.uptimeMillis();
+        customHandler.postDelayed(updateTimerThread, 0);
+
+        //vibration sourced from http://stackoverflow.com/questions/13950338/how-to-make-an-android-device-vibrate
+        Vibrator playMatch = (Vibrator) GameLogic.this.getSystemService(Context.VIBRATOR_SERVICE);
+        // Vibrate for 500 milliseconds
+        playMatch.vibrate(300);
+
         //get a random number between 0 and 3 and create a toast accordingly
         int randomNum = 0 + (int)(Math.random() * 3);
 
         //show to the user what they have to do
         Toast.makeText(GameLogic.this, gameOptions[randomNum], Toast.LENGTH_LONG).show();
+
+        // Instantiate the gesture detector with the
+        // application context and an implementation of
+        // GestureDetector.OnGestureListener
+        mDetector = new GestureDetector(this, this);
+        // Set the gesture detector as the double tap
+        // listener.
+        mDetector.setOnDoubleTapListener(this);
+
+
+
     }
+
+    //run the timer
+    //runnable timer sourced from http://examples.javacodegeeks.com/android/core/os/handler/android-timer-example/
+    private Runnable updateTimerThread = new Runnable()
+    {
+        public void run()
+        {
+            //milliseconds calculation
+            timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
+
+            //calculate updated time
+            updatedTime = timeSwapBuff + timeInMilliseconds;
+
+            //seconds is equal to updatedtime in milliseconds * 1000
+            int secs = (int) (updatedTime / 1000);
+            //to get mins then seconds * 60 is equal to mins
+            int mins = secs / 60;
+            secs = secs % 60;
+
+            //update global variables
+            minutes = mins;
+
+            Log.d("Time", "Mins: " + minutes);
+
+            customHandler.postDelayed(this, 0);
+        }
+
+    };
 
     //run method that starts the game and updates everything in the game
     public void run()
@@ -47,22 +119,35 @@ public class GameLogic extends AppCompatActivity implements GestureDetector.OnDo
 
     @Override
     public boolean onSingleTapConfirmed(MotionEvent e) {
-        return false;
+
+        Log.d("E: " , "Single Tap " + e);
+
+        //show to the user what they have done
+        Toast.makeText(GameLogic.this, "Single Tap", Toast.LENGTH_LONG).show();
+
+        return true;
     }
 
     @Override
     public boolean onDoubleTap(MotionEvent e) {
-        return false;
+
+        Log.d("E: " , "Double Tap " + e);
+
+        //show to the user what they have done
+        Toast.makeText(GameLogic.this, "Double Tap", Toast.LENGTH_LONG).show();
+
+        return true;
     }
 
     @Override
     public boolean onDoubleTapEvent(MotionEvent e) {
-        return false;
+
+        return true;
     }
 
     @Override
     public boolean onDown(MotionEvent e) {
-        return false;
+        return true;
     }
 
     @Override
@@ -72,7 +157,7 @@ public class GameLogic extends AppCompatActivity implements GestureDetector.OnDo
 
     @Override
     public boolean onSingleTapUp(MotionEvent e) {
-        return false;
+        return true;
     }
 
     @Override
@@ -83,23 +168,32 @@ public class GameLogic extends AppCompatActivity implements GestureDetector.OnDo
     @Override
     public void onLongPress(MotionEvent e) {
 
+        Log.d("E: " , "Long Tap " + e);
+        //show to the user what they have done
+        Toast.makeText(GameLogic.this, "Long Pressed", Toast.LENGTH_LONG).show();
+
     }
 
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
 
+        Log.d("E: " , "Swipe Tap " + e1 + " E2:" + e2);
+
         //make sure that the x-axis variable has a significant value
         //if a person moves their finger right the x-axis is updated and vice versa for y-axis
         if(velocityX > 100.000)
         {
-
+            //show to the user what they have done
+            Toast.makeText(GameLogic.this, "Swiped Right", Toast.LENGTH_LONG).show();
         }
         //make sure that the x-axis variable has a significant value
         else if(velocityX < 100.000)
         {
-
+            //show to the user what they have done
+            Toast.makeText(GameLogic.this, "Swiped Left", Toast.LENGTH_LONG).show();
         }
-        return false;
+
+        return true;
     }
 
     @Override
